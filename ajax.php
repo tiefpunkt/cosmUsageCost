a852937974a6ba348d21b2ab4515506f8ee06eea
Severin Schols <severin@schols.de>
2012-06-17 16:45:25 +0200

    CO2 output tracking, Summary on top

--- ajax.php
+++ ajax.php
@@ -14,24 +14,40 @@ $json = $pachube->getDatastreamHistory("json", $config->feed, $config->datastrea
 
 $datastream = json_decode($json);
 
-$output;
+$days;
 $current_value = 0;
+$total_usage = 0;
+$counter = 0;
 //print_r($datastream["datapoints"]);
 foreach ($datastream->datapoints as $datapoint) {
 	if ($current_value != 0) {
 		$date = substr($datapoint->at,0,10);
-		$output[$date]["usage"] = $datapoint->value - $current_value;
-		if ($output[$date]["usage"] >= 0) {
-			$output[$date]["cost"] = round($output[$date]["usage"] * 0.215,2);
-			$output[$date]["usage"] = round($output[$date]["usage"],2);
+		$days[$date]["usage"] = $datapoint->value - $current_value;
+		if ($days[$date]["usage"] >= 0) {
+			$total_usage += $days[$date]["usage"];
+			$days[$date]["cost"] = round($days[$date]["usage"] * $config->price,2);
+			$days[$date]["co2"] = round($days[$date]["usage"] * $config->co2equivalents,0);
+			$days[$date]["usage"] = round($days[$date]["usage"],2);
+			$counter++;
 		} else {
-			$output[$date]["usage"] = "n/a";
-			$output[$date]["cost"] = "n/a";
+			$days[$date]["usage"] = "n/a";
+			$days[$date]["cost"] = "n/a";
+			$days[$date]["co2"] = "n/a";
 		}
 	}
 	$current_value = $datapoint->value;
 }
 
+$output["days"] = $days;
+$output["detail"]["price"] = $config->price;
+$output["detail"]["co2equivalents"] = $config->co2equivalents;
+$output["summary"]["total"]["usage"] = round($total_usage,2);
+$output["summary"]["total"]["cost"] = round($total_usage * $config->price,2);
+$output["summary"]["total"]["co2"] = round($total_usage * $config->co2equivalents,0);
+$avg_usage = $total_usage / $counter;
+$output["summary"]["avg"]["usage"] = round($avg_usage,2);
+$output["summary"]["avg"]["cost"] = round($avg_usage * $config->price,2);
+$output["summary"]["avg"]["co2"] = round($avg_usage * $config->co2equivalents,0);
 
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 19 Jul 1997 00:00:00 GMT');
