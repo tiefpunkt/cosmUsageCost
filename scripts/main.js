a0fee7c685812836d4b1a4bd2c24f4d62e9da879
Severin Schols <severin@schols.de>
2012-06-20 01:53:31 +0200

    Date and Number format

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
 	$.ajax({
 		url: 'ajax.php',
 		success: function(data) {
-			console.log(data);
 			output = "";
 			for (var datapoint in data.days) {
-				output += "<tr><td>" + datapoint + "</td><td>" + data.days[datapoint].usage + "</td><td>" + data.days[datapoint].cost + "</td><td>" + data.days[datapoint].co2 + "</td></tr>";
+				date = moment(datapoint, "YYYY-MM-DD").format("ddd, MMM Do");
+				output += "<tr><td>" + date + "</td><td>" + data.days[datapoint].usage + "</td><td>" + data.days[datapoint].cost + "</td><td>" + $.format.number(data.days[datapoint].co2, "#,##0") + "</td></tr>";
 			}
 			output = "<table>" + output + "</table>";
 			$("#output").html(output);
 			$("#total_usage").html(data.summary.total.usage + " kWh");
 			$("#total_cost").html(data.summary.total.cost + " €");
-			$("#total_co2").html(data.summary.total.co2 + " g");
+			$("#total_co2").html($.format.number(data.summary.total.co2, "#,##0") + " g");
 			$("#avg_usage").html(data.summary.avg.usage + " kWh");
 			$("#avg_cost").html(data.summary.avg.cost + " €");
-			$("#avg_co2").html(data.summary.avg.co2 + " g");
+			$("#avg_co2").html($.format.number(data.summary.avg.co2, "#,##0") + " g");
 			$("#detail_price").html(data.detail.price + " €/kWh");
 			$("#detail_co2").html(data.detail.co2equivalents + " g/kWh");
 		}
